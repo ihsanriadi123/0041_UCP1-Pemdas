221714ad46e1a905dc3f28ef9542f88358104fd8
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-05-05 16:54:00 +0700

    awal pengomitan

---
+++ Exercise1.cpp
@@ -0,0 +1,68 @@
+// m.ihsan putra riadi 20220140041
+
+// 1. String, integer, Class
+// 2. contoh implementasi prosedur
+//       void display () {
+//    cout << "Panjangnya = " << panjang << endl;
+//    cout << "lebarnya = " << lebar << endl;
+//    cout << "luasnya = " << hitungluas << endl;
+//  }
+//    contoh implemetasi fungsi 
+//         void input() {
+//              cout << "masukkan bilangan pertama = ";
+//              cin >> bilsatu;
+//              cout << "Masukkan bilangan kedua = ";
+//              cin >> bildua;
+// 3. implementasi dari looping 
+//           void tampildata() {
+//    cout << "NIM \t: "<< nim << endl;
+//    cout << "Nama \t: "<< nama << endl;
+//    cout << "Umur \t: "<< umur << endl;
+//  }
+
+// 4. impelementasi dari conditional startement
+//          if (rerata >= 60 || Matematika > 70) {
+//          status = "Lulus";
+// 5. implementasi dari sturct
+//        int main() {
+//    mahasiswa mhs[3];
+//    for (int i=0;i<3;i++) {
+//        cout <<"data ke-" <<(i+1) <<":" << endl;
+//        cout << "nomor mahasiswa: ";
+//        getline(cin,mhs.nim);
+//        cout << "nama mahasiswa: ";
+//        getline(cin,mhs.nama);
+
+
+// Soal Nomor 6
+
+#include <iostream>
+using namespace std;
+
+
+string status (int sensor1, int sensor2) {
+    cout << "Masukkan indeks : ";
+    cin >> sensor1;
+    cout << "Masukkan kualitas udara : ";
+    cin >> sensor2;
+};
+
+string status = (int sensor1 + int sensor2);
+
+if (indeks >= 75 || sensor1 > 45) {
+	status = "sehat";
+}
+else {
+	status = "Tidak sehat";
+}
+
+int main () {
+   string kota() = (tanggerang, status)
+    int sensor1 (75, 40);
+    int sensor2 (75, 45);
+
+cout << "Status sehat : " << sehat << endl;
+cout << "Status tidak sehat : " << tidak sehat << endl;
+
+    return 0;
+};
